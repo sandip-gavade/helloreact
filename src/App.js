fe0868d4578e2f6210b0c6421bf2a8661d3265bc
sandip-gavade <gavadesandip81@gmail.com>
2021-09-17 04:19:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+        return <h1>Hello React catching you soon </h1>;
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+
+// let el=<h1>Hello React I will catch you </h1>;
+
+// ReactDOM.render(el,document.getElementById("root"));
+
+ReactDOM.render(<App/>,document.getElementById("root"));
